2f0a5791889e7fe9dd70e835c44b1f8e9d1f5e1c
marci98p <96825333+marci98p@users.noreply.github.com>
2025-04-14 13:14:47 +0200

    Update Vorlesung.cpp

--- Vorlesung.cpp
+++ Vorlesung.cpp
@@ -61,3 +61,15 @@ cout << "Manager: " << (*dept2Person1.getManager()).getName() << endl;          
 Employee* departmentManager1 = new Employee("Michael Mouse", 60000, nullptr);        // wiht new
 // must be deleted after use
 delete departmentManger1;        // Caling delete on a pointer to an object runs the destructor!
+
+
+// this pointer - example
+class Test {
+  public:
+    int x = 12;
+    void print();
+ };
+ void Test::print() {
+  cout << "x         =" << x          << endl;        // option 1
+  cout << "(*this).x =" << (*this).x  << endl;        // option 2
+  cout << "this->x   =" << this->x    << endl;        // option 3
