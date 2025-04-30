bb1f0e2ecf9b395f18c99ce3b674902db0c9cfcc
marci98p <96825333+marci98p@users.noreply.github.com>
2025-04-30 14:53:01 +0200

    Update Vorlesung.cpp

--- Vorlesung.cpp
+++ Vorlesung.cpp
@@ -15,3 +15,55 @@ class Test {
   cout << "x         =" << x          << endl;        // option 1
   cout << "(*this).x =" << (*this).x  << endl;        // option 2
   cout << "this->x   =" << this->x    << endl;        // option 3
+
+
+
+// ************************** 30.04.******************************
+
+// new & delete
+ int main() {
+   Employee* manager = new Employee("Michael Mouse", 60000, nullptr);    // allocate a new employee; new returns ptr ! therefore employee must be a ptr adress
+   … 
+  // deallocate the Employee
+   delete manager;
+   // the pointer still points to the memory address
+   Employee worker("Dorith Duck", 40000, manager);
+   
+   return 0;
+   //pointer manager automatically deallocated at end of main function
+ }
+
+// this ptr
+// returs the ptr-adrs of current object
+
+// *this returns current object
+class Calc {
+ public:
+    Calc() { value_ = 0; }
+    Calc& addValue(int value);
+    Calc& subValue(int value);
+    Calc& multValue(int value);
+    int getValue() { return value_; }
+ private:
+    int value_;
+ };
+
+Calc& Calc::addValue(int value)  {
+   value_ += value;
+   return *this;
+ }
+ Calc& Calc::subValue(int value) {
+   value_ -= value;
+   return *this;
+ }
+ Calc& Calc::multValue(int value) {
+   value_ *= value;
+   return *this;
+ }
+// to do something like this => member function chain
+calc.addValue(5).subValue(3).getValue()
+
+
+// friend
+  
+
