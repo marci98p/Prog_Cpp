40eec771516f0b02212f51cc18b49e0c0a11dae6
marci98p <96825333+marci98p@users.noreply.github.com>
2025-06-30 19:44:41 +0200

    Update 1st_Sem.cpp

--- 1st_Sem.cpp
+++ 1st_Sem.cpp
@@ -300,21 +300,22 @@ std:: vector<type> vec;				    // declares a vector as empty vector
 std:: vector<type> vec (n);			  // declares a vector with n elements
 std:: vector<type> vec (n, val);	// all n elements initialized with val
 
-vec[n];                  // get value of vector with index n
-vec.at(n);               // vec[n] but with range check
-vec = vector2;           // copy vector
-vec.size();              // get array size
-vec.capacity();          // get number of elements of vec pondon:"sizeof();"
-vec.reserved();          // reserves minimum amount of mememory for n elements
-vec.push_back(val);      // adds val to end of vec
-vec.pop_back();          // removes last element of vec
-vec.front();             // get first value of vec
-vec.back();              // get last value of vec
-vec.begin();             // get interator to first element (similar to pointer)
-vec.end();               // get interator to last element (similar to pointer)
-vec.erase(n);            // removes element with iterator n from vec
-vec.insert(n, val);      // insert element with val at iterator n
-vec.clear();             // removes all element of vec
+vec[n];                        // get value of vector with index n
+vec.at(n);                     // vec[n] but with range check
+vec = vector2;                 // copy vector
+vec.size();                    // get array size
+vec.capacity();                // get number of elements of vec pondon:"sizeof();"
+vec.reserved();                // reserves minimum amount of mememory for n elements
+vec.push_back(val);            // adds val to end of vec
+vec.pop_back();                // removes last element of vec
+vec.front();                   // get first value of vec
+vec.back();                    // get last value of vec
+vec.begin();                   // get interator to first element (similar to pointer)
+vec.end();                     // get interator to last element (similar to pointer)
+vec.erase(n);                  // removes element with iterator n from vec
+vec.insert(vec.begin(), val);  //  insert item on first place
+vec.insert(n, val);            // insert element with val at iterator n
+vec.clear();                   // removes all element of vec
 
 // ________________________________________________________________________________________________
 // ##### Functions ################################################################################
