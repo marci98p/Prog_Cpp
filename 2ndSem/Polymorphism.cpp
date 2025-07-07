07137f6302c9421467a5222d7fb11da51089eabd
marci98p <96825333+marci98p@users.noreply.github.com>
2025-07-07 22:32:19 +0200

    Update Polymorphism.cpp

--- 2ndSem/Polymorphism.cpp
+++ 2ndSem/Polymorphism.cpp
@@ -9,23 +9,86 @@ virtual   // declare functions in parent class with virtual
 class Animal {
 public:
     virtual ~Animal();               // destructor needs to be virual as well 
-    virtual void speak() const {
-        std::cout << "Animal sound\n";
-    }
+    virtual void speak();            // virtual function declaration 
+};
+void Animal::speak() {               // function definition - no virtual needed
+    td::cout << "Animal sound\n";
+}
+
+class Dog : public Animal {          // Inheritance
+public:
+    ~Dog();                  
+    void speak() override;           // override: compiler comand to override parent memberfunction
+};
+void Dog::speak() override {    // override: compiler comand to override parent memberfunction
+    std::cout << "Woof!\n";
+}
+
+int main() {
+    Dog doggo;
+    Animal& animal_ref = doggo;    // doggo as animal&
+    animal_ref.makeSound();        // ➜  Dog::speak()       if speak() ain't virtual ➜  Animal::speak();
+    
+    Dog& dog_ref = doggo;          // doggo as dog&
+    dog_ref.makeSound();           // ➜  Dog::speak()
+ }
+
+// static cast => see below
+int main(){
+    Dog doggo;
+    Animal& animal_ref = doggo;
+    Dog& dog_ref = static_cast<Dog&> (animal_ref);
+    dog_ref.makeSound();            // ➜  Dog::speak()
+ }
+// dangerous cast (animal is no dog)
+int main(){
+    Animal doggo;                    // here's the difference
+    Animal& animal_ref = doggo;
+    Dog& dog_ref = static_cast<Dog&> (animal_ref);
+    dog_ref.makeSound();            // ➜  Animal::speak()
+ }
+
+
+// Example with ptr
+int main(){
+    Animal* a = new Dog;       // same tbt
+    Animal* a = new Dog();     // same tbt
+
+    a->speak();     // because of `virtual`  ➜  Dog::speak()
+    delete a;       // if virtual ~Animal(); ain't virual ➜  ~Animal() would be called
+}
+
+
+// *************************** Pure virtual ➜ Abstract Class ***************************
+class Animal {
+public:
+    virtual ~Animal();               // destructor needs to be virual as well 
+    virtual void speak() = 0;        // add = 0 ➜ function definition removed! 
 };
 
 class Dog : public Animal {          // Inheritance
 public:
     ~Dog();                  
-    void speak() const override {    // override: compiler comand to override parent memberfunction
-        std::cout << "Woof!\n";
-    }
+    void speak() override;           // override: compiler comand to override parent memberfunction
 };
-
-int main(){
-  Animal* a = new Dog(); 
-  a->speak();  // because of `virtual`  ➜  Dog::speak()
-
-  delete a;       // if virtual ~Animal(); ain't virual ➜  ~Animal() would be called
+void Dog::speak() override {         // overriding function must be given now
+    std::cout << "Woof!\n";
 }
 
+
+// *************************** static / dynamic casting ***************************
+for (Animal* animal : animals) {
+   Dog* dog = static_cast<Dog*>(animal);        // static
+   Cat* cat = dynamic_cast<Cat*>(animal);       // dynamic
+   if (dog!=nullptr) dog->wag();
+   if (cat!=nullptr) cat->purr();
+ }
+
+// static cast
+// ➜ happens at compile time
+// ➜ checks if typecast is possible
+
+// dynamic cast
+// ➜ safe downcasting for polymorphic
+// ➜ treating a ptr of the base class as a ptr of the derived class
+// ➜ returs a nullptr at runtime, if cast is not possible
