a554904080a971c041257e765aab306b6a6ef460
marci98p <96825333+marci98p@users.noreply.github.com>
2025-07-17 19:59:48 +0200

    Update stringstream.cpp

--- 1stSem/stringstream.cpp
+++ 1stSem/stringstream.cpp
@@ -1,8 +1,10 @@
-string Shape::getShapeInfo() const{
-    // "A <name> at position <position>"
-    double x = position_.getX();
-    double y = position_.getY();
-    stringstream ss;                // stringstream needs to be initialisied first
-    ss << "A " << name_ << " at position (" << x << "," << y << ")";
-    return ss.str();
-}
+// best for strings only
+return (firstname_ + " " + lastname_);  
+
+
+
+// best for multiple types (no casting neccessary - type converting via << )
+stringstream ss;                // stringstream needs to be initialisied first
+ss << "A " << name_ << " at position (" << x << "," << y << ")";
+return ss.str();
+
