5144b169ff72db5fcbb97112c7bb530b61d4bd21
marci98p <96825333+marci98p@users.noreply.github.com>
2025-07-27 17:20:46 +0200

    Update stringstream.cpp

--- 1stSem/stringstream.cpp
+++ 1stSem/stringstream.cpp
@@ -1,8 +1,6 @@
 // best for strings only
 return (firstname_ + " " + lastname_);  
 
-
-
 // best for multiple types (no casting neccessary - type converting via << )
 stringstream ss;                // stringstream needs to be initialisied first
 ss << "A " << name_ << " at position (" << x << "," << y << ")";
